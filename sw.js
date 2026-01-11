707ee45df8bd743e6238be212188064cd93195e3
Light <lightoftheonlystar@gmail.com>
2026-01-11 22:12:27 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,33 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "atma-calculator-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open("atma-calculator").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", e => {
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
   );
