806212b905bb400f61d7b9073ffe615691851aef
Light <lightoftheonlystar@gmail.com>
2026-01-11 22:39:50 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,40 @@
-const CACHE_NAME = "atma-calculator-v2";
+const CACHE_NAME = "atma-calculator-v3";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon.png"
+  "./icon.png",
+  "https://fonts.googleapis.com/css2?family=UnifrakturCook&display=swap"
 ];
 
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
 
-self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      })
+      .catch(() => caches.match(e.request))
   );
 });
