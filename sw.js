4fc8e959425e229f7977e0a8e094c68ac5c5f40c
Light <lightoftheonlystar@gmail.com>
2026-01-11 20:54:25 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("atma-calculator").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
